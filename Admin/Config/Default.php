25813279dc74bcb3c1354df02f81fb27e0535323
sunjuntong <1216765268@qq.com>
2018-09-23 14:47:30 +0800

    1

---
+++ Admin/Config/Default.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * 基础配置
+ */
+return array(
+    /**
+     * App是否关闭
+     */
+    'CLOSE_APP' => false,
+    
+    /**
+     * Debug模式
+     */
+    'DEBUG' => false,
+    
+    /**
+     * 是否自动更新
+     */
+    'AUTO_UPDATE' => false,
+    
+    /**
+     * 记录日志
+     */
+    'RECORD_LOG' => false,
+    
+    /**
+     * 默认语言包
+     */
+    'LANGUAGE' => 'cn',
+    
+    /**
+     * 默认时区
+     */
+    'TIME_ZONE' => 'Asia/Shanghai',
+    
+    /**
+     * 是否显示视图
+     */
+    'Enable_View' => true,
+    
+    /**
+     * 视图后缀
+     */
+    'View_Suffix' => 'html', // html,tpl
+    
+    /**
+     * 路径前缀
+     */
+    'Path_Prefix' => '',
+    
+    /**
+     * 资源路径
+     */
+    'Path_Public' => '/Public',
+);
