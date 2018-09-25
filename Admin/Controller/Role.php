2429b34276da4b10dea3be0dcb39f59228a9e057
sunjuntong <1216765268@qq.com>
2018-09-25 20:14:23 +0800

    1

---
+++ Admin/Controller/Role.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018 年 9 月 24 日 0024
+ * Time: 19:24:18
+ */
+
+class Controller_Role extends Controller_Base{
+
+    public function userlist(){
+        $this->_page_title = "用户管理";
+    }
+}
