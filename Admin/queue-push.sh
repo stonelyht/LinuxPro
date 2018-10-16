aac0cfeb34ef3c0749e40a6b7b9ba155a02b7807
sunjuntong <1216765268@qq.com>
2018-10-16 14:14:25 +0800

    1

--- Admin/queue-push.sh
+++ Admin/queue-push.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-
 step=1 #间隔的秒数，不能大于60
 
-for (( i = 0; i < 60; i=(i+step) )); do
-    $(curl 'https://www.stonelyshop.cn/queue.php')
-    sleep $step
-done
+for((i=0;i<60;i=(i+step))); do
 
+	$(curl 'https://www.stonelyshop.cn/queue.php')
+
+	sleep $step
+
+done
 exit 0
