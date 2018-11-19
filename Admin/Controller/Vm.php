41e82858a00ec94d7847224187f5c2bdf47745b7
sunjuntong <1216765268@qq.com>
2018-11-19 17:00:41 +0800

    1

---
+++ Admin/Controller/Vm.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/11/19
+ * Time: 16:54
+ */
+
+class Controller_Vm extends Controller_Base{
+
+    public function index(){
+        
+    }
+}
